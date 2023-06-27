98fbea11873a593d42e50775a36e58f34f2ed623
inacio augusto fernandes <134444252+inacioas@users.noreply.github.com>
2023-06-27 18:16:56 -0300

    Projeto cadastro de clientes com enumerações e composição

---
+++ src/entities/Order.java
@@ -0,0 +1,90 @@
+package entities;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import entities.enums.OrderStatus;
+
+public class Order {
+	
+	SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+	
+	private Date moment;
+	private OrderStatus status;
+	private Client client;
+	
+	List<OrderItem> order = new ArrayList<>();
+	
+	public Order() {
+		
+	}
+
+	public Order(Date moment, OrderStatus status, Client client) {
+		this.moment = moment;
+		this.status = status;
+		this.client = client;
+	}
+	
+	public Client getClient() {
+		return client;
+	}
+
+	public void setClient(Client client) {
+		this.client = client;
+	}
+
+	public List<OrderItem> getOrder() {
+		return order;
+	}
+
+	public Date getMoment() {
+		return moment;
+	}
+
+	public void setMoment(Date moment) {
+		this.moment = moment;
+	}
+
+	public OrderStatus getStatus() {
+		return status;
+	}
+
+	public void setStatus(OrderStatus status) {
+		this.status = status;
+	}
+	
+	public void addItem(OrderItem item) {
+		order.add(item);
+	}
+	
+	public void romoveItem(OrderItem item) {
+		order.remove(item);
+	}
+	
+	public double total() {
+		double sum = 0.0;
+		for (OrderItem it : order) {
+			sum += it.subTotal();
+		}
+		return sum;
+	}
+	
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append("Order moment: ");
+		sb.append(sdf.format(moment) + "\n");
+		sb.append("Order status: ");
+		sb.append(status + "\n");
+		sb.append("Client: ");
+		sb.append(client + "\n");
+		sb.append("Order items:\n");
+		for (OrderItem item : order) {
+			sb.append(item + "\n");
+		}
+		sb.append("Total price: $");
+		sb.append(String.format("%.2f", total()));
+		return sb.toString();
+	}	
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author inaci
+ *
+ */
+module ExercicioJava {
+}
